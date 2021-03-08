fc538225a8d01a6e82d9be1b919c2ff19bb0b973
EienHanabi <yumehanabi@foxmail.com>
2021-03-08 15:08:22 +0800

    Add !search command

---
+++ Operations/search.py
@@ -0,0 +1,72 @@
+from Arcapi import SyncApi
+import numpy as np
+import random
+import discord
+import time
+
+difficulty = ['PST', 'PRS', 'FTR', 'BYD']
+clear_type = ['Failed', 'Normal Clear', 'Full Recall', 'Pure Memory', 'Easy Clear', 'Hard Clear']
+cover = 'http://119.23.30.103:8080/ArcAssets/cover/'
+ava = 'http://119.23.30.103:8080/ArcAssets/icon/'
+
+def checktime(timestamp):
+    sec = int(time.time() - timestamp/1000)
+    if(sec < 60):
+        return str(sec) + 's'
+    elif(sec < 3600):
+        return str(int(sec/60)) + 'm'
+    elif(sec < 86400):
+        return str(int(sec/3600)) + 'h'
+    else:
+        return str(int(sec/86400)) + 'd'
+
+def search(usercode, song_name, song_difficulty='FTR'):
+    api_ = SyncApi(user_code=usercode)
+    arraybuffer = api_.scores(start=8, end=12)
+    personal_info = arraybuffer[1]
+    song_info = arraybuffer[0]
+    best_info = []
+    for i in range(len(arraybuffer)):
+        if i > 1:
+            best_info.append(arraybuffer[i])
+    best_info.sort(key=lambda info: info['rating'], reverse=True)
+    
+    result = []
+    for i in best_info:
+        if song_info[i['song_id']]['en'].lower().startswith(song_name.lower())\
+                and difficulty[i['difficulty']].lower() == song_difficulty.lower():
+            result.append(i)
+
+    if len(result) == 0:
+        return False
+
+    response = random.choice(result)
+    
+    try:
+        song_name = song_info[response['song_id']]['en']
+    except KeyError:
+        song_name = response['song_id']
+
+    if personal_info['is_char_uncapped']:
+        ava_url = ava + str(personal_info['character']) + 'u_icon.png'
+    else:
+        ava_url = ava + str(personal_info['character']) + '_icon.png'
+
+    rating = list(str(personal_info['rating']))
+    rating.insert(np.size(rating) - 2, '.')
+    rating_str = "".join(rating)
+
+    response_embed = discord.Embed(title=song_name+ '[' + str(difficulty[response['difficulty']]) + ']',
+                                  type='rich' ,
+                    description='**' + str(response['score']) + '**(' + clear_type[response['clear_type']] + ')\n**▸Pure: **' + \
+                    str(response['perfect_count']) + '(+' + str(response['shiny_perfect_count']) + ') **▸Far: **' + \
+                    str(response['near_count']) + ' **▸Lost: **' + str(response['miss_count']) + '\n**▸Potential: **' + \
+                    str(response['constant']) + '->**' + str(response['rating'])[0:6] +'**',
+                                  color= discord.Color.magenta())
+
+    response_embed.set_thumbnail(url =cover + response['song_id'] + '.jpg')
+    response_embed.set_author(name= personal_info['name'] + ' [' +  rating_str + ']', icon_url=ava_url)
+    response_embed.set_footer(text= 'Played ' + checktime(response['time_played']) + ' ago')
+
+    return response_embed
+    
--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ from Operations.kou import kou
 from Operations.recent import recent
 from Operations.target import target
 from Operations.b30o import b30o
+from Operations.search import search
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -31,6 +32,7 @@ async def on_message(message):
         if contentdata[0] == '!help':
             response = '**!bind <UID>** --- Binding discord ID with your Arcaea UID\n' \
                        '**!recent** -- Show your recent play\n' \
+                       '**!search <SongName(match by prefix)> <Difficulty>** -- Search your score on this chart (Difficulty is Optional, FTR by default)\n' \
                        '**!b30** -- Show your best 30 plays in a picture\n' \
                        '**!b30o** -- Show your best 30 plays before v3.0\n' \
                        '**!target** -- Randomly select a target chart which might improve your potential\n' \
@@ -79,6 +81,31 @@ async def on_message(message):
                     return
             response = 'Please bind your ID first, use !bind <UID>'
             await message.channel.send(response)
+
+        elif contentdata[0] == '!search':
+            players = np.loadtxt('list.txt', dtype=str, comments='&')
+            if(len(contentdata) < 2):
+                response = 'Missing Option, use !search (SongName) <Difficulty>\n(Diffuculty is optional, FTR by default)'
+                await message.channel.send(content=message.author.mention + '\n' + response)
+                return
+
+            for i in players:
+                if i[0] == str(message.author.id):
+                    if(len(contentdata) < 3):
+                        response = search(i[1], contentdata[1])
+                    else:
+                        response = search(i[1], contentdata[1], contentdata[2])
+
+                    if not response:
+                        response = 'No matches found'
+                        await message.channel.send(content=message.author.mention + '\n' + response)
+                        return
+                    else:
+                        await message.channel.send(content=message.author.mention, embed=response)
+                        return
+
+            response = 'Please bind your ID first, use !bind <UID>'
+            await message.channel.send(content=message.author.mention + '\n' + response)
 
         elif contentdata[0] == '!b30':
             players = np.loadtxt('list.txt', dtype=str, comments='&')
